91fd090813a680c964760ebe53874a477d298ea1
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-05 15:45:22 -0400

    Initial commit

---
+++ src/main/java/me/rallings/springbootjpadata/SpringbootJpaDataApplication.java
@@ -0,0 +1,13 @@
+package me.rallings.springbootjpadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rallings/springbootjpadata/SpringbootJpaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rallings.springbootjpadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
